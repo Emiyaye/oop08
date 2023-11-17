e21220dc97c54e40379b1ece04fe5baecc7bff5a
Botti Bao <botti.bao@studio.unibo.it>
2023-11-17 21:59:34 +0100

    Started writeDeathCause method, totally bugged

--- 83-tdd-deathnote/src/main/java/it/unibo/deathnote/Impl/DeathNoteImpl.java
+++ 83-tdd-deathnote/src/main/java/it/unibo/deathnote/Impl/DeathNoteImpl.java
@@ -1,6 +1,8 @@
 package it.unibo.deathnote.Impl;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,23 +11,24 @@ import it.unibo.deathnote.api.DeathNote;
 
 public class DeathNoteImpl implements DeathNote{
 
-    final private static int DEATH_DETAILS = 1;
-    final private static int DEATH_CAUSE = 0;
+    final private static int DEATH_DETAILS_POSITION = 1;
+    final private static int DEATH_CAUSE_POSITION= 0;
     final private static int DEATH_CAUSE_LIMIT = 40;
     final private static int DEATH_DETAILS_LIMIT = 6040;
     final static private List<String> INITAL_DEATH_CAUSE_DETAILS = new ArrayList<>();
-    final static private String DEAFAULT_DEATH = "Heart Attach";
+    final static private String DEAFAULT_DEATH = "Heart Attack";
     final static private String EMPTY = "";
     final static private Map<Integer, String> RULES = new HashMap<>();
     final private Map<String, List<String>> deathNote = new HashMap<>();
-    private int currentTime;
+    private String cachedName;
+    private long writeTime;
 
     public DeathNoteImpl(){
         for (int i = 1; i <= DeathNote.RULES.size(); i++){
             RULES.put(i, DeathNote.RULES.get(i - 1));
         }
-        INITAL_DEATH_CAUSE_DETAILS.add(DEATH_CAUSE, EMPTY);
-        INITAL_DEATH_CAUSE_DETAILS.add(DEATH_DETAILS, EMPTY);
+        INITAL_DEATH_CAUSE_DETAILS.add(DEATH_CAUSE_POSITION, EMPTY);
+        INITAL_DEATH_CAUSE_DETAILS.add(DEATH_DETAILS_POSITION, EMPTY);
     }
 
     @Override
@@ -36,8 +39,15 @@ public class DeathNoteImpl implements DeathNote{
         return RULES.get(ruleNumber);
     }
 
+    private boolean deathCauseStillEmpty(String name){
+        return (name != this.cachedName && deathNote.get(this.cachedName).get(DEATH_CAUSE_POSITION) == EMPTY);
+    }
+
     @Override
     public void writeName(String name) {
+        if(deathCauseStillEmpty(name)){
+            deathNote.get(this.cachedName).set(DEATH_CAUSE_POSITION, DEAFAULT_DEATH);
+        }
         if (name == null){
             throw new NullPointerException("You cannot kill a null");
         }
@@ -45,12 +55,18 @@ public class DeathNoteImpl implements DeathNote{
             throw new IllegalArgumentException("You cannot kill the guy twice");
         }
         this.deathNote.put(name, new ArrayList<String> (INITAL_DEATH_CAUSE_DETAILS));
-        
+        this.cachedName = name;
+        this.writeTime = System.currentTimeMillis();
     }
 
     @Override
     public boolean writeDeathCause(String cause) {
-        
+        final long currTime = System.currentTimeMillis();
+        if (currTime - this.writeTime < DEATH_CAUSE_LIMIT){
+            this.deathNote.get(this.cachedName).set(DEATH_CAUSE_POSITION, cause);
+            return true;
+        }
+        if ()
         return true;
     }
 
@@ -65,7 +81,7 @@ public class DeathNoteImpl implements DeathNote{
         if (!this.deathNote.containsKey(name)){
             throw new IllegalArgumentException("Cannot find the person");
         }
-        return this.deathNote.get(name).get(DEATH_CAUSE);
+        return this.deathNote.get(name).get(DEATH_CAUSE_POSITION);
     }
 
     @Override
@@ -73,7 +89,7 @@ public class DeathNoteImpl implements DeathNote{
         if (!this.deathNote.containsKey(name)){
             throw new IllegalArgumentException("Cannot find the person");
         }
-        return this.deathNote.get(name).get(DEATH_DETAILS);
+        return this.deathNote.get(name).get(DEATH_DETAILS_POSITION);
     }
 
     @Override
